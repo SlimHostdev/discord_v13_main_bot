2fc0265786548c674b1fad96fcc17499f23c364c
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-01-16 21:28:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ client.once("ready", () => {
     console.log([`Bot Preffix: ${process.env.PREFFIX}`]);
     console.log([`Bot Collor: ${process.env.COLLOR}`]);
     console.log([`Bot Invite:]`]);
-    console.log(`[\x1b[31m ${process.env.INVITE} \x1b[0m]`);
+    console.log([`[\x1b[31m ${process.env.INVITE} \x1b[0m]`]);
     console.log('<---------------------------------------------------------------------------------------------------------------------->',);
 
 });
