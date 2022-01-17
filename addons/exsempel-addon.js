72104c173fac6de73d007f457ed11daf6861a0be
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-01-17 22:36:05 +0100

    Create exsempel-addon.js

---
+++ addons/exsempel-addon.js
@@ -0,0 +1,25 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    var botEmbed = new discord.MessageEmbed()
+        .setTitle("exsempel addon !!!!!!")
+        .setDescription("Dit is een command om de addons te testen")
+        .setColor(process.env.COLLOR)
+        .setThumbnail(process.env.LOGO)
+        //.setImage(''process.env.INFO'')
+        .setTimestamp()
+        .setFooter("exsempel addon command")
+        .addFields(
+            { name: "Bot Naam", value: client.user.username }
+        )
+
+    return message.channel.send({ embeds: [botEmbed] });
+
+}
+
+module.exports.help = {
+    name: "exsempel",
+    category: "add ons",
+    discription: "Dist is een voorbeeld code voor addons."
+}
