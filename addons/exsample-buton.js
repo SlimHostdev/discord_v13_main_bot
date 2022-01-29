8634c696a3d1f312c31d76b8025fba36470461e0
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-01-29 06:15:17 +0100

    add buttons

---
+++ addons/exsample-buton.js
@@ -0,0 +1,59 @@
+const discord = require("discord.js");const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    const danger = discord.MessageActionRow().addCompoments(
+
+        new discord.MessageButton()
+        .setCustomId("test")
+        .setLabel("TEST")
+        .setStyle("DANGER")
+
+    );
+
+    const link = discord.MessageActionRow().addCompoments(
+
+        new discord.MessageButton()
+        .setCustomId("test")
+        .setLabel("TEST")
+        .setStyle("LINK")
+        .setURL("https://slimgame.nl")
+
+    );
+
+    const primary = discord.MessageActionRow().addCompoments(
+
+        new discord.MessageButton()
+        .setCustomId("test")
+        .setLabel("TEST")
+        .setStyle("PRIMARY")
+
+    );
+
+    const sacondary = discord.MessageActionRow().addCompoments(
+
+        new discord.MessageButton()
+        .setCustomId("test")
+        .setLabel("TEST")
+        .setStyle("SACONDARY")
+
+    );
+
+    const success = discord.MessageActionRow().addCompoments(
+
+        new discord.MessageButton()
+        .setCustomId("test")
+        .setLabel("TEST")
+        .setStyle("SUCCESS")
+
+    );
+
+    message.channel.send({ content: "Test Message", Compoments: [danger, link, primary, sacondary, success] });
+
+}
+
+module.exports.help = {
+    name: "buton",
+    category: "add ons",
+    discription: "Dist is an example code for buton addons."
+}
