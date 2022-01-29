797cec4c509622871c807b4827ce4065cc250d0f
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-01-29 08:47:19 +0100

    Update status.js

--- admincommands/status.js
+++ admincommands/status.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
 
     client.user.setPresence({
 
-        status: "online",
+        status: "dnd",
         activities: [
             {
                 name: statusTxt
