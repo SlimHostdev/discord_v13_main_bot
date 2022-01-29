3cdd448a5f045bd9aa3f2a253f1dbf074826ebeb
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-01-29 06:50:27 +0100

    Update exsample-buton.js

--- addons/exsample-buton.js
+++ addons/exsample-buton.js
@@ -2,58 +2,42 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    const danger = new discord.MessageActionRow().addComponents(
+    const row = new discord.MessageActionRow().addComponents(
 
         new discord.MessageButton()
         .setCustomId("test")
         .setLabel("TEST")
         .setStyle("DANGER")
-        .setDisabled(true)
-    
-    );
-    
-    const link = new discord.MessageActionRow().addComponents(
-    
+        .setDisabled(true),
+
         new discord.MessageButton()
         .setCustomId("test")
         .setLabel("TEST")
         .setStyle("LINK")
         .setURL("https://slimgame.nl")
-        .setDisabled(true)
-    
-    );
-    
-    const primary = new discord.MessageActionRow().addComponents(
-    
+        .setDisabled(true),
+
         new discord.MessageButton()
         .setCustomId("test")
         .setLabel("TEST")
         .setStyle("PRIMARY")
-        .setDisabled(true)
-    
-    );
-    
-    const sacondary = new discord.MessageActionRow().addComponents(
-    
+        .setDisabled(true),
+
         new discord.MessageButton()
         .setCustomId("test")
         .setLabel("TEST")
         .setStyle("SACONDARY")
-        .setDisabled(true)
-    
-    );
-    
-    const success = new discord.MessageActionRow().addComponents(
-    
+        .setDisabled(true),
+
         new discord.MessageButton()
         .setCustomId("test")
         .setLabel("TEST")
         .setStyle("SUCCESS")
         .setDisabled(true)
-    
+
     );
 
-    message.channel.send({ content: "Test Message", components: [danger] });
+    message.channel.send({ content: "Test Message", components: [row] });
 
 }
 
