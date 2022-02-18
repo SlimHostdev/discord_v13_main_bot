0acf81ebd5e9bf7de40f1996a4488106d5b58646
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-02-18 22:05:54 +0100

    Update close.js

--- admincommands/close.js
+++ admincommands/close.js
@@ -9,6 +9,12 @@ module.exports.run = async (client, message, args) => {
 
     const categoryID = process.env.TICKETID;
 
+    var reason = args.join(" ");
+    if (!reason) return message.channel.send("You must give a reason for closeing the ticket!").then(msg => {
+        message.delete()
+        setTimeout(() => msg.delete(), 10000);
+    });
+
     if (message.channel.parentId == categoryID) {
 
         message.channel.delete();
@@ -20,7 +26,8 @@ module.exports.run = async (client, message, args) => {
             .setDescription("This ticket is closed!")
             .addFields(
                 { name: "Ticket Name:", value: message.channel.name, inline: false },
-                { name: "Ticket Clost By:", value: message.author.username, inline: false }
+                { name: "Ticket Clost By:", value: message.author.username, inline: false },
+                { name: "Reason:", value: reason, inline: false }
             )
             .setTimestamp()
             .setFooter("Ticket closed");
