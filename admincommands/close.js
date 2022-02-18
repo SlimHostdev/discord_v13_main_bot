5d53ba886bc7b98a6171a9720534ec4bf3dac05d
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-02-18 21:42:36 +0100

    Update close.js

--- admincommands/close.js
+++ admincommands/close.js
@@ -7,9 +7,11 @@ module.exports.run = async (client, message, args) => {
         setTimeout(() => msg.delete(), 10000);
     });
 
-//    const categoryID = process.env.TICKETID;
+    const categoryID = process.env.TICKETID;
 
-    if (message.channel.parendId == process.env.TICKETID) {
+    if (message.channel.parendId == categoryID) {
+
+        message.channel.delete();
 
         var embedTicket = new discord.MessageEmbed()
             .setTitle("Ticket, " + message.channel.name)
