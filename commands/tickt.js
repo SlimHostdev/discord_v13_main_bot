855bdad8136f9bc5af08d4ecd759448b344d360c
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-02-18 19:10:42 +0100

    Tecket UPDATE 1.0.0
    Tecket UPDATE 1.0.0

---
+++ commands/tickt.js
@@ -0,0 +1,97 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    const categoryID = process.env.TICKETID
+
+    var userName = message.author.username;
+
+    var userDiscriminator = message.author.discriminator;
+
+    var reason = args.join(" ");
+    if (!reason) return message.channel.send("You must give a reason for the ticket!");
+
+    var ticketBestaat = false;
+
+    message.guild.channels.cache.forEach((channel) => {
+
+        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
+
+            message.channel.send("You already have a Ticket that is open!");
+
+            ticketBestaat = true;
+
+            return;
+
+        }
+
+    });
+
+    if (ticketBestaat) return;
+
+    message.guild.channels.create(userName.toLowerCase() + "-" + userDiscriminator, { type: "text" }).then((createChan) => {
+
+        createChan.setParent(categoryID).then((settedParent) => {
+
+            // Perms zodat iedereen niets kan lezen.
+            settedParent.permissionOverwrites.edit(message.guild.roles.cache.find(x => x.name === "@everyone"), {
+
+                SEND_MESSAGES: false,
+                VIEW_CHANNEL: false
+
+            });
+
+            // READ_MESSAGE_HISTORY Was vroeger READ_MESSAGES
+            // Perms zodat de gebruiker die het command heeft getypt alles kan zien van zijn ticket.
+            settedParent.permissionOverwrites.edit(message.author.id, {
+                CREATE_INSTANT_INVITE: false,
+                READ_MESSAGE_HISTORY: true,
+                SEND_MESSAGES: true,
+                ATTACH_FILES: true,
+                CONNECT: true,
+                ADD_REACTIONS: true
+            });
+
+            // Perms zodat de gebruikers die admin zijn alles kunnen zien van zijn ticket.
+            settedParent.permissionOverwrites.edit(message.guild.roles.cache.find(x => x.id === `${process.env.ADMINROLL}`), {
+                CREATE_INSTANT_INVITE: false,
+                READ_MESSAGE_HISTORY: true,
+                SEND_MESSAGES: true,
+                ATTACH_FILES: true,
+                CONNECT: true,
+                ADD_REACTIONS: true
+            });
+
+            let today = new Date();
+            let dd = String(today.getDate()).padStart(2, '0'); // Nul toevoegen als het bv. 1 is -> 01
+            let mm = String(today.getMonth() + 1).padStart(2, '0');
+            let yyyy = today.getFullYear();
+            today = `${dd}/${mm}/${yyyy}`;
+
+            let embedParent = new discord.MessageEmbed()
+                .setAuthor(message.author.username, message.author.displayAvatarURL({ size: 4096 }))
+                .setTitle('New Ticket.')
+                .addFields(
+                    { name: "Reason:", value: reason, inline: true },
+                    { name: "Create On:", value: today, inline: true }
+                );
+
+            message.channel.send('✅ Your ticket has been created.');
+
+            settedParent.send({ embeds: [embedParent] });
+
+        }).catch(err => {
+            message.channel.send('❌ There is an error when creating your Ticket. **Contact an ADMIN**')
+        })
+
+    }).catch(err => {
+        message.channel.send('❌ There is an error when creating your Ticket. **Contact an ADMIN**')
+    });
+
+}
+
+module.exports.help = {
+    name: "ticket",
+    category: "info",
+    discription: "With this command you can create a ticket."
+}
