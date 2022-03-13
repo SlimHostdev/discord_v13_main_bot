be5e61e606a93cb080ec16a897a5e7c0d400264a
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-03-13 17:51:39 +0100

    Update ping.js

--- admincommands/ping.js
+++ admincommands/ping.js
@@ -31,5 +31,5 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
     name: "ping",
     category: "admin",
-    discription: `${language.cmd_ping_disc}`
+    discription: "This is a command to test the bot."
 }
