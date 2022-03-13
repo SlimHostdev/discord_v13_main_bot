f18e68daabc3547d9ac5a9c5a7fc8aed9543e9a5
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-03-13 21:50:28 +0100

    .

--- admincommands/ban.js
+++ admincommands/ban.js
@@ -1,27 +1,31 @@
 const discord = require("discord.js");
+//File server
+const fs = require("fs");
+//Taal van de bot
+const language = JSON.parse(fs.readFileSync(`./locale/${process.env.LANGUAGE}.json`, "utf-8"));
 
 module.exports.run = async (client, message, args) => {
 
     //Log chat
     const adminlog = message.member.guild.channels.cache.get(process.env.ADMINLOGS);
     
-    if (!message.member.roles.cache.has(`${process.env.ADMINROLL}`)) return message.reply("You're Not an ADMIN so you can't do this.");
+    if (!message.member.roles.cache.has(`${process.env.ADMINROLL}`)) return message.reply(`${language.no_admin}`);
 
     //(prefix)ban naam reden
 
-    if (!message.guild.me.permissions.has("BAN_MEMBERS")) return message.reply("I have no right to Ban anyone.");
+    if (!message.guild.me.permissions.has("BAN_MEMBERS")) return message.reply(`${language.cmd_ban_no_purm}`);
 
-    if (!args[0]) return message.reply("You must specify a person.");
+    if (!args[0]) return message.reply(`${language.cmd_ban_no_specify}`);
 
-    if (!args[1]) return message.reply("You must indicate why you want to Ban the person.");
+    if (!args[1]) return message.reply(`${language.cmd_ban_no_reason}`);
 
     //try catch
 
     var banUser = message.guild.members.cache.get(message.mentions.users.first().id || message.guild.members.get(args[0]).id)
 
-    if (!banUser) return message.reply("I can't find anyone in the server with this name");
+    if (!banUser) return message.reply(`${language.cmd_ban_cant_find_user}`);
 
-    if (banUser.roles.cache.has(`${process.env.ADMINROLL}`)) return message.reply("You can't Ban a ADMIN!");
+    if (banUser.roles.cache.has(`${process.env.ADMINROLL}`)) return message.reply(`${language.cmd_ban_cant_ban_admin}`);
 
     var reason = args.slice(1).join(" ");
 
@@ -29,8 +33,8 @@ module.exports.run = async (client, message, args) => {
         .setThumbnail(process.env.LOGO)
         .setColor(process.env.WARNCOLLOR)
         .setImage(process.env.BANNER)
-        .setTitle("Are you sure you want to do this Ban?")
-        .setDescription(`Do you want to Ban ${banUser} ?`)
+        .setTitle(`${language.cmd_ban_sure_title}`)
+        .setDescription(`${language.cmd_ban_sure_disc} ${banUser} ${language.cmd_ban_sure_disc2}?`)
         .setTimestamp()
 
     var embedBan = new discord.MessageEmbed()
