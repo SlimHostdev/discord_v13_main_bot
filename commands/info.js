3197d6fe76a3033484c13f3531cc341e0f4d0ddc
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-03-15 01:16:41 +0100

    info cmd language update

--- commands/info.js
+++ commands/info.js
@@ -9,18 +9,18 @@ const language = JSON.parse(fs.readFileSync(`./language/${process.env.LANGUAGE}.
 module.exports.run = async (client, message, args) => {
 
     var botEmbed = new discord.MessageEmbed()
-        .setTitle("Info command")
-        .setDescription("Here you will find a lot of info from the bot")
+        .setTitle(`${language.cmd_info_title}`)
+        .setDescription(`${language.cmd_info_disc}`)
         .setColor(process.env.COLLOR)
         .setThumbnail(process.env.LOGO)
         .setImage(process.env.BANNER)
         .setTimestamp()
-        .setFooter("info command")
-        .addField("Bot Naam", client.user.username)
+        .setFooter(`${language.cmd_info_footer}`)
+        .addField(`${language.cmd_info_bot_name}`, client.user.username)
         .addFields(
-            { name: "Bot Prefix", value: process.env.PREFFIX },
-            { name: "Bot Dev", value: `${packege.author}` },
-            { name: "Bot Host", value: `${packege.slimhost}` }
+            { name: `${language.cmd_info_bot_prefix}`, value: process.env.PREFFIX },
+            { name: `${language.cmd_info_bot_author}`, value: `${packege.author}` },
+            { name: `${language.cmd_info_bot_host}`, value: `${packege.slimhost}` }
         )
 
         const row = new discord.MessageActionRow().addComponents(
@@ -43,5 +43,5 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
     name: "info",
     category: "info",
-    discription: "Here you will find a lot of info from the bot."
+    discription: language.cmd_info_disc
 }
