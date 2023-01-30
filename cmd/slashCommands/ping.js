5bed630550c22c0b9797cb41e937662d81f27c47
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2023-01-30 16:35:10 +0100

    update taal "Slashcmd_ping_msg"

--- cmd/slashCommands/ping.js
+++ cmd/slashCommands/ping.js
@@ -1,5 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+const discord = require("discord.js");
+//File server
+const fs = require("fs");
+//Taal van de bot
+const language = JSON.parse(fs.readFileSync(`./language/${process.env.LANGUAGES}.json`, "utf-8"));
+
 module.exports = {
 
     data: new SlashCommandBuilder()
@@ -7,7 +13,7 @@ module.exports = {
         .setDescription("Dit is een Test CMD."),
         async execute(client, interaction) {
 
-            interaction.reply({content: `Pong ${client.ws.ping} ms.`, ephemeral: true});
+            interaction.reply({content: `${language.Slashcmd_ping_msg} ${client.ws.ping} ms.`, ephemeral: true});
 
         }
 
