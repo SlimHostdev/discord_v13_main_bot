67f4f86aa60e6170848ddd2c75cce1a43244b533
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2023-07-11 01:14:49 +0200

    Create ticket.js

---
+++ cmd/slashCommands/ticket.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ticket')
+    .setDescription('Maakt een supportticket aan.'),
+  async execute(interaction) {
+    // Controleren of de gebruiker de categorie kan beheren
+    const categoryPermissions = interaction.guild.roles.cache
+      .find(role => role.id === ${process.env.TICKETSTAFF}`)
+      .permissions
+      .has(Permissions.FLAGS.MANAGE_CHANNELS);
+    
+    if (!categoryPermissions) {
+      return await interaction.reply('Je hebt geen toestemming om een kanaal in deze categorie te maken.');
+    }
+
+    // Ticketkanaal maken
+    const category = interaction.guild.channels.cache.find(channel => channel.name === 'Support Tickets' && channel.type === 'GUILD_CATEGORY');
+    if (!category) {
+      return await interaction.reply('Categorie "Support Tickets" niet gevonden.');
+    }
+
+    const channel = await interaction.guild.channels.create('Ticket', {
+      type: 'GUILD_TEXT',
+      parent: category.id,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.roles.everyone,
+          deny: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+          id: interaction.member,
+          allow: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+      ],
+    });
+
+    await interaction.reply(`Supportticket aangemaakt in ${channel}.`);
+  },
+};
