646d942df1ccffe65292bad407f2a07694da7f9a
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2023-12-27 16:49:36 +0100

    Update botinfo.js

--- cmd/slashCommands/botinfo.js
+++ cmd/slashCommands/botinfo.js
@@ -1,21 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Client, MessageEmbed } = require('discord.js');
+
 const discord = require("discord.js");
+// Bestandssysteem
+const fs = require("fs");
+// Taal van de bot
+const language = JSON.parse(fs.readFileSync(`./language/${process.env.LANGUAGES}.json`, 'utf-8'));
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('botinfo')
     .setDescription('Toont informatie over de bot.'),
-  async execute(interaction) {
-    const botUser = client.user;
+  async execute(client, interaction) {
 
     const embed = new MessageEmbed()
       .setTitle('Botinformatie')
       .setDescription('Hier is wat informatie over de bot:')
       .setColor('#0099ff')
-      .addField('Naam', botUser.username)
-      .addField('Tag', botUser.tag)
-      .addField('ID', botUser.id)
+      .addField('Naam', client.user.username)
+      .addField(`${language.Slashcmd_ping_msg} ${client.ws.ping} ms.`)
       .addField('Gemaakt op', botUser.createdAt.toDateString());
 
     await interaction.reply({ embeds: [embed] });
