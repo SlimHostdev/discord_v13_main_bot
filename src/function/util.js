8c0105c8cd786935615462244cf0f915440cce07
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2024-07-22 14:16:10 +0200

    Update util.js

--- src/function/util.js
+++ src/function/util.js
@@ -28,7 +28,7 @@ function getTimestamp() {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  return `${hours}:${minutes}:${seconds} ~ ${year}-${month}-${day}`;
+  return `${hours}:${minutes}:${seconds} ~ ${day}-${month}-${year}`;
 }
 
 function parse(message) {
