c89a2d7b7226d086aa0572c0f9b5c9e61a67c854
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2024-07-22 17:10:07 +0200

    Update ping.js

--- cmd/admin/adminSlashCommands/ping.js
+++ cmd/admin/adminSlashCommands/ping.js
@@ -45,7 +45,7 @@ module.exports = {
       let cmdUserName = interaction.user.tag;
       let cmdUserId = interaction.user.id;
       log.info(
-        `Ping cmd is gedaan door [ USER: ${cmdUserName} ID: ${cmdUserId}] de bot heeft [ ${client.ws.ping}ms ] ping!`
+        `${interaction.name} cmd is gedaan door [ USER: ${cmdUserName} ID: ${cmdUserId}]`
       );
     });
   },
