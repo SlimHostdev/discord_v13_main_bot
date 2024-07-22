845a0857a378b54990608bf526ff679e71f0981f
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2024-07-22 15:01:00 +0200

    Update ping.js

--- cmd/admin/admincommands/ping.js
+++ cmd/admin/admincommands/ping.js
@@ -1,4 +1,5 @@
 const discord = require("discord.js");
+const log = require("./src/function/util");
 //File server
 const fs = require("fs");
 //Taal van de bot
@@ -11,6 +12,7 @@ module.exports.run = async (client, message, args) => {
     return message.reply(`${language.no_admin}`);
 
   const m = await message.channel.send("Ping?");
+  let ms = m.setTimestamp - message.createdTimestamp;
 
   var botEmbed = new discord.MessageEmbed()
     .setTitle(`${language.cmd_ping_title}`)
@@ -24,11 +26,13 @@ module.exports.run = async (client, message, args) => {
       { name: `${language.cmd_ping_name}`, value: client.user.username },
       {
         name: `${language.cmd_ping_latency}`,
-        value: `${m.createdTimestamp - message.createdTimestamp}ms.`,
+        value: `${ms}ms.`,
+        //value: `${m.createdTimestamp - message.createdTimestamp}ms.`,
       }
     );
 
   return message.channel.send({ embeds: [botEmbed] }).then((msg) => {
+    log.custom(`Ping command is gebruikt\n ${ms}ms.`);
     message.delete();
     m.delete();
     setTimeout(() => msg.delete(), 10000);
