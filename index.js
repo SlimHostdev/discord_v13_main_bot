f3de8d271208db75b22ee3cbdc0d327132a29a4b
Bryan <81651393+L3G3CLAN@users.noreply.github.com>
2024-08-13 18:42:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ client.once("ready", () => {
   };
   updateStatus();
 
-  if (process.env.DBHOST === "") {
+  if (process.env.DBHOST === " ") {
     DBHOST = "https://" + DBHOST + ":" + 3306;
   } else {
     DBHOST = "Not In Use 🚫- to use this feature you need to set it up in";
