09ce34d8301b9ba176252d343c80cec2aa2adb83
Bryan <info@l3g3clan.nl>
2024-10-24 13:09:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,31 +35,21 @@ let DBPW = atob(process.env.DBPW);
 let tokenDecode = atob(process.env.TOKEN);
 
 const DB = mysql.createConnection({
-  host: DBHOST,
-  user: DBUSER,
-  password: DBPW,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPW,
   database: process.env.DBNAME,
 });
 
-DB.connect(function (err) {
-  //if (err) throw err;
-
-  log.info(`Connected To DataBase!`);
-
-  //Table Create
-  // var sql = "CREATE TABLE SERVERINFO (JOIN_ROLL_ID VARCHAR(255), WELKOM_ID VARCHAR(255))";
-
-  // DB.query(sql, function (err, result) {
-  //   if (err) throw err;
-  //   log.created(`[\x1b[31m Create Table! \x1b[0m]`);
-  //});
-
-  //Select Form Table
-  //    DB.query("SELECT JOIN_ROLL_ID FROM SERVERINFO", function (err, joinrollid, fields) {
-  //        if (err) throw err;
-  //        log.info(joinrollid);
-  //    });
-});
+const connectDatabase = async () => {
+  try {
+    await DB.connect();
+    logFormatted("Connected To DataBase!");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+};
+connectDatabase();
 
 //File server
 const fs = require("fs");
